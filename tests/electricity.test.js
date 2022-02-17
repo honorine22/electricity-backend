2db6d8ae53aa15c6845b65e09ff878f86bd85791
honorine22 <higiranez@gmail.com>
2022-02-17 17:39:12 +0200

    Added config

--- tests/electricity.test.js
+++ tests/electricity.test.js
@@ -25,7 +25,7 @@ describe('electricity bill', () => {
                     expect(res.body).to.have.property('message').to.deep.equal('electricity bills retrieved successfully!')
                     // expect(res.body).to.have.property('data').to.be.a('array');
                     expect(res).to.have.status(200)
-                    // expect(res.body.data[0].title).to.eql('')
+                    // expect(res.body.data[0].amount).to.eql('')
                     // expect(res.body).length.to.be.equal(0)
                     done();
                 });
@@ -38,8 +38,8 @@ describe('electricity bill', () => {
     describe('/POST electricity bill', () => {
         it('it should not POST electricity bill without pages field', (done) => {
             let newBill = {
-                name: "Learn and present Nodejs Testing",
-                completed: false
+                meter: "Learn and present Nodejs Testing",
+                token: false
             }
             chai.request(server)
                 .post('/electricity')
@@ -53,9 +53,9 @@ describe('electricity bill', () => {
 
         it('it should POST electricity bill ', (done) => {
             let newBill = {
-                title: "Club Meeting",
-                name: "Attending peace Club",
-                completed: true
+                amount: "Club Meeting",
+                meter: "Attending peace Club",
+                token: true
             }
             chai.request(server)
                 .post('/electricity')
@@ -64,9 +64,9 @@ describe('electricity bill', () => {
                     expect(res.status).to.equal(201);
                     expect(res.body).to.be.a('object');
                     expect(res.body).to.have.property('message').to.deep.equal('bill successfully added!');
-                    expect(res.body.newBill).to.have.property('title')
-                    expect(res.body.newBill).to.have.property('name')
-                    expect(res.body.newBill).to.have.property('completed')
+                    expect(res.body.newBill).to.have.property('amount')
+                    expect(res.body.newBill).to.have.property('meter')
+                    expect(res.body.newBill).to.have.property('token')
                     done();
                 });
         });
@@ -75,7 +75,7 @@ describe('electricity bill', () => {
     // Get, update, and delete by id
     describe('/electricity/:billId', () => {
         it('it should GET electricity bill by the given id', (done) => {
-            let bill = new Bill({ title: 'Club Meeting', name: 'Attending peace Club', completed: true })
+            let bill = new Bill({ amount: 'Club Meeting', meter: 'Attending peace Club', token: true })
             bill.save((err, bill) => {
                 chai.request(server).get('/electricity/', + bill.id).send(bill)
                     .end((err, res) => {
@@ -90,17 +90,17 @@ describe('electricity bill', () => {
 
     describe('/PUT/:id electricity bill', () => {
         it('it should UPDATE a bill given the id', (done) => {
-            let bill = new Bill({ title: "Learning", name: "Never stop learning!", completed: false })
+            let bill = new Bill({ amount: "Learning", meter: "Never stop learning!", token: false })
             bill.save((err, bill) => {
                 chai.request(server)
                     .put('/electricity/' + bill.id)
-                    .send({ title: "Learning", name: "Never stop learning!", completed: false })
+                    .send({ amount: "Learning", meter: "Never stop learning!", token: false })
                     .end((err, res) => {
                         expect(res.body).to.be.a('object');
                         expect(res.body).to.have.property('message').to.deep.equal('bill updated!');
-                        expect(res.body.updatedbill).to.have.property('title').to.deep.equal('Learning')
-                        expect(res.body.updatedbill).to.have.property('name').to.deep.equal('Never stop learning!')
-                        expect(res.body.updatedbill).to.have.property('completed').to.deep.equal(false)
+                        expect(res.body.updatedbill).to.have.property('amount').to.deep.equal('Learning')
+                        expect(res.body.updatedbill).to.have.property('meter').to.deep.equal('Never stop learning!')
+                        expect(res.body.updatedbill).to.have.property('token').to.deep.equal(false)
                         done();
                     });
             });
@@ -112,7 +112,7 @@ describe('electricity bill', () => {
  */
     describe('/DELETE/:id electricity bill', () => {
         it('it should DELETE electricity bill given the id', (done) => {
-            let bill = new Bill({ title: "Learning", name: "Never stop learning!", completed: false })
+            let bill = new Bill({ amount: "Learning", meter: "Never stop learning!", token: false })
             bill.save((err, bill) => {
                 chai.request(server)
                     .delete('/electricity/' + bill.id)
